093aa6640c7893fb7e5ee6e5def24a8f0155a877
KevinSizz <kevinskate.kg@gmail.com>
2020-04-15 00:53:28 -0500

    .

---
+++ Components/Datos.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react'
+import {View, Text, StyleSheet, TouchableOpacity, Button} from 'react-native'
+
+class Datos extends Component{
+
+
+    form = () =>{
+        this.props.navigation.navigate('Form')
+    }
+
+
+
+    render(){
+        //const { navigation } = this.props;
+        return(
+
+            <View style={Styles.container}>
+                <View style={Styles.header}>
+                    <Button title="Volver al Form" onPress={this.form}>
+                    </Button>
+                </View>
+
+                <View style={Styles.header}>
+                    <Text >
+                        NumeroLista: {JSON.stringify(this.props.navigation.getParam('NumeroLista','NO-ID'))}
+                    </Text>
+                    <Text>
+                        UsuarioLista: {(this.props.navigation.getParam('UsuarioLista','default value'))}
+                    </Text>
+                </View>
+            </View>
+            
+        )
+    }
+
+}
+
+
+const Styles=StyleSheet.create({
+
+    container :{
+        flex : 1,
+        backgroundColor : '#E4F2F8',
+        padding: 10,
+        flexDirection:'column',
+        justifyContent:'space-between'
+      },
+    
+    
+      header: {
+        flex: 1,
+        //flexDirection:'column',
+        alignItems : 'flex-start',
+        //justifyContent: 'space-between'
+    
+      },
+
+
+})
+export default Datos
--- Components/Iniciar.js
+++ Components/Iniciar.js
@@ -1,7 +1,40 @@
 import React, {Component} from 'react'
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import Constants from 'expo-constants';
+import {View, 
+  Text, 
+  StyleSheet, 
+  TouchableOpacity,
+  SafeAreaView,
+  FlatList
+} from 'react-native'
+
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+function Item({ title }) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
 
 class Add extends Component{
+
+
 
   agregar = () =>{
     this.props.navigation.navigate('Form')
@@ -21,6 +54,15 @@ class Add extends Component{
               Añade los cuartos que posees y los cuales quieres controlar. 
             </Text>
           </View>
+
+
+          <SafeAreaView style={styles.containerr}>
+            <FlatList
+              data={DATA}
+              renderItem={({ item }) => <Item title={item.title} />}
+              keyExtractor={item => item.id}
+            />
+          </SafeAreaView>
 
 
           <View style = {Styles.foot}>    
@@ -44,12 +86,35 @@ class Add extends Component{
           </View>
           
 
+
+
         </View>
+
+
+
       )
 
     }
 }
 const Styles = StyleSheet.create({
+
+  containerr: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+
+
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+
+
+  title: {
+    fontSize: 32,
+  },
 
   container :{
     flex : 1,
