aa0212c538ac393104c45c6a03e1e664e88b1c6e
KevinSizz <kevinskate.kg@gmail.com>
2020-04-15 18:44:54 -0500

    h

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {View, Text, StyleSheet, Button, Alert} from 'react-native'
 import {  createAppContainer} from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-import {Icon} from 'native-base' 
+//import { Icon } from 'react-native-elements'
 import {createStackNavigator} from 'react-navigation-stack'
 
 //Screens
