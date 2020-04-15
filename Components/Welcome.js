57f7c41bc9a2606f877e88d87129fccc35273e39
KevinSizz <kevinskate.kg@gmail.com>
2020-04-15 18:36:43 -0500

    i

--- Components/Welcome.js
+++ Components/Welcome.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
-//import {Icon} from 'native-base' 
+import { Icon } from 'react-native-elements' 
 
 class Welcome extends Component{
 
@@ -16,7 +16,7 @@ class Welcome extends Component{
           <View style = {Styles.header}>
             <Text style= {Styles.paragraph}> 
               BIENVENIDO
-
+              <Icon name='home' />
             </Text>
           </View>
 
