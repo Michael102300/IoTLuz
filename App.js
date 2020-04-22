5c83eef62032c08ff3e603a1434373b7c76fa466
KevinSizz <kevinskate.kg@gmail.com>
2020-04-21 20:02:56 -0500

    .

--- App.js
+++ App.js
@@ -59,3 +59,35 @@ export default createAppContainer(createBottomTabNavigator(
 
 
 
+
+/* {
+  "expo": {
+    "name": "IoTluz",
+    "slug": "IoTluz",
+    "platforms": [
+      "ios",
+      "android",
+      "web"
+    ],
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "splash": {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
+    },
+    "updates": {
+      "fallbackToCacheTimeout": 0
+    },
+    "assetBundlePatterns": [
+      "Iba aqui :v"
+    ],
+    "ios": {
+      "supportsTablet": true
+    }
+  }
+} */
+
+
+//"**/*"
